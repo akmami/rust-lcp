bf2723991f5e51df94d621fc0efddf9184b1e62e
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-09-17 15:20:49 +0300

    test case for cmp and eq for core operators written

--- src/tests.rs
+++ src/tests.rs
@@ -146,7 +146,7 @@ fn test_core_compression() {
         assert_eq!(core2.get_start_index(), 3);
         assert_eq!(core2.get_blocks(), [0b10001]);
 
-        println!("Compression btw core1 and core2 completed successfully.")
+        println!("Compression btw core1 and core2 completed successfully.");
 
         let core3: Core = Core::new(1, 6, "A");
         let mut core4: Core = Core::new(2, 8, "TAAAA");
@@ -165,7 +165,7 @@ fn test_core_compression() {
         assert_eq!(core4.get_start_index(), 5);
         assert_eq!(core4.get_blocks(), [0b100]);
 
-        println!("Compression btw core3 and core4 completed successfully.")
+        println!("Compression btw core3 and core4 completed successfully.");
 
         let core5: Core = Core::new(1, 6, "T");
         let mut core6: Core = Core::new(2, 8, "TAAAA");
@@ -184,7 +184,7 @@ fn test_core_compression() {
         assert_eq!(core6.get_start_index(), 6);
         assert_eq!(core6.get_blocks(), [0b00]);
 
-        println!("Compression btw core5 and core6 completed successfully.")
+        println!("Compression btw core5 and core6 completed successfully.");
 
         let core7: Core = Core::new(1, 6, "C");
         let mut core8: Core = Core::new(2, 8, "T");
@@ -203,8 +203,94 @@ fn test_core_compression() {
         assert_eq!(core8.get_start_index(), 6);
         assert_eq!(core8.get_blocks(), [0b11]);
 
-        println!("Compression btw core7 and core8 completed successfully.")
+        println!("Compression btw core7 and core8 completed successfully.");
         
+    }
+    //drop(guard);
+}
+
+
+#[test]
+fn test_core_comparison_eq() {
+    //let guard = mtx.lock().unwrap();
+
+    // A/a=0, T/t=3, G/g=2, C/c=1
+    unsafe {
+        let verbose = true;
+        init_coefficients_default(verbose);
+
+        let core1: Core = Core::new(1, 6, "ATGTGCT");
+        let core2: Core = Core::new(2, 8, "ATGTGCT");
+        
+        assert_eq!(core1 == core2, true);
+
+        let core3: Core = Core::new(1, 6, "T");
+        let core4: Core = Core::new(2, 8, "TTTT");
+
+        assert_eq!(core3 == core4, false);
+
+        let core5: Core = Core::new(1, 6, "A");
+        let core6: Core = Core::new(2, 8, "AAAA");
+
+        assert_eq!(core5 == core6, false);
+
+        let core7: Core = Core::new(1, 6, "CC");
+        let core8: Core = Core::new(2, 8, "CC");
+
+        assert_eq!(core7 == core8, true);
+
+        let core9: Core = Core::new(1, 6, "TT");
+        let core10: Core = Core::new(2, 8, "TT");
+
+        assert_eq!(core9 != core10, false);
+
+        let core11: Core = Core::new(1, 6, "ATGGCT");
+        let core12: Core = Core::new(2, 8, "ATGTGCT");
+        
+        assert_eq!(core11 != core12, true);
+    }
+    //drop(guard);
+}
+
+
+#[test]
+fn test_core_comparison_cmp() {
+    //let guard = mtx.lock().unwrap();
+
+    // A/a=0, T/t=3, G/g=2, C/c=1
+    unsafe {
+        let verbose = true;
+        init_coefficients_default(verbose);
+
+        let core1: Core = Core::new(1, 6, "ATGTGCT");
+        let core2: Core = Core::new(2, 8, "ATGTGCT");
+        
+        assert_eq!(core1 < core2, false);
+
+        let core3: Core = Core::new(1, 6, "ATGTGCT");
+        let core4: Core = Core::new(2, 8, "ATGTGCT");
+
+        assert_eq!(core3 <= core4, true);
+
+        let core5: Core = Core::new(1, 6, "A");
+        let core6: Core = Core::new(2, 8, "AAAAA");
+
+        assert_eq!(core5 < core6, true);
+
+        let core7: Core = Core::new(1, 6, "TC");
+        let core8: Core = Core::new(2, 8, "CC");
+
+        assert_eq!(core7 > core8, true);
+
+        let core9: Core = Core::new(1, 6, "TC");
+        let core10: Core = Core::new(2, 8, "CC");
+
+        assert_eq!(core9 >= core10, true);
+
+        let core11: Core = Core::new(1, 6, "AGTGCT");
+        let core12: Core = Core::new(2, 8, "ATGTGCT");
+        
+        assert_eq!(core11 > core12, false);
     }
     //drop(guard);
 }
